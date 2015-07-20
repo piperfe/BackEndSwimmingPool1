0489dcfc17b321044c9d9a0c3c936fbc3975b8d7
falfaro <falfaro@lan.com>
2015-07-19 20:50:11 -0400

    add embed tomcat and integrationTests

---
+++ SwimmingPoolServiceExample/src/test/java/integrationTest/EmbeddedTomcat.java
@@ -0,0 +1,58 @@
+package integrationTest;
+
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class EmbeddedTomcat {
+
+    private Tomcat tomcat = new Tomcat();
+
+    static {
+//        System.setProperty(Constants.ROOT_APPENDER_PROP, System.getProperty(Constants.ROOT_APPENDER_PROP,"consoleAppender"));
+    }
+    final String context = System.getProperty("application.context", "SwimmingPool/rest");
+
+    public void start() {
+        try {
+            String baseDir = ".";
+            tomcat.setPort(0);
+            tomcat.setBaseDir(baseDir);
+            tomcat.getHost().setAppBase(baseDir);
+            tomcat.getHost().setDeployOnStartup(true);
+            tomcat.getHost().setAutoDeploy(true);
+            tomcat.start();
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+        }
+
+        deploy(context);
+    }
+
+    public void stop() {
+        try {
+            tomcat.stop();
+            tomcat.destroy();
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deploy(String contextName) {
+        tomcat.addWebapp(tomcat.getHost(), "/" + contextName, "src/main/webapp");
+    }
+
+    public String getUrl() {
+        return String.format("http://%s:%d/%s", getHost(), getPort(), context);
+    }
+
+    public String getHost() {
+        return tomcat.getHost().getName();
+    }
+
+    public int getPort() {
+        return tomcat.getConnector().getLocalPort();
+    }
+}
